f6a090883001281437585b78d451529b3ea529fc
Aleksei2576 <palguev.alexey@gmail.com>
2024-02-14 08:34:01 +0300

    commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const nodemailer = require('nodemailer')
+const cors = require('cors');
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+
+
+app.post('/send-email', (req, res) => {
+	const { name, phone } = req.body
+
+	const transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: 'palguev.alexey@gmail.com',
+			pass: 'wupqybqsloeccfon'
+		}
+	})
+
+	const mailOptions = {
+		from: 'AV Style',
+		to: 'palguev.alexey@gmail.com',
+		text: `Name: ${name}\nPhone: ${phone}\n`			
+		}
+
+	transporter.sendMail(mailOptions, function(error, info){
+		if (error) {
+			console.log(error);
+			res.status(500).json('Error sending email')
+		} else {
+			console.log('Email sent: ' + info.response)
+			res.status(200).json('Email sent successfully')
+		}
+	})
+})
+
+app.listen(3334, () => {
+    console.log('Application listening on port 3333!')
+})
